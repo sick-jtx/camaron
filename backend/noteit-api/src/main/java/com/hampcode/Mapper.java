aa7e553e2549301c52c735bda45edaf5c0b37e4e
marco <jitrix15@hotmail.com>
2019-09-24 16:55:49 -0500

    asd

---
+++ backend/noteit-api/src/main/java/com/hampcode/Mapper.java
@@ -0,0 +1,70 @@
+package com.hampcode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.hampcode.api.viewmodel.FeedbackViewModel;
+import com.hampcode.api.viewmodel.NoteViewModel;
+import com.hampcode.api.viewmodel.NotebookViewModel;
+import com.hampcode.model.Feedback;
+import com.hampcode.model.Note;
+import com.hampcode.model.Notebook;
+import com.hampcode.repository.NotebookRepository;
+
+@Component
+public class Mapper {
+
+	@Autowired
+	private NotebookRepository notebookRepository;
+
+	public NoteViewModel convertToNoteViewModel(Note entity) {
+		NoteViewModel viewModel = new NoteViewModel();
+		viewModel.setTitle(entity.getTitle());
+		viewModel.setId(entity.getId());
+		viewModel.setLastModifiedOn(entity.getLastModifiedOn());
+		viewModel.setText(entity.getText());
+		viewModel.setNotebookId(entity.getNotebook().getId());
+
+		return viewModel;
+	}
+
+	public Note convertToNoteEntity(NoteViewModel viewModel) {
+		Notebook notebook = this.notebookRepository.findById(viewModel.getNotebookId()).get();
+		Note entity = new Note(viewModel.getId(), viewModel.getTitle(), viewModel.getText(), notebook);
+
+		return entity;
+	}
+
+	public NotebookViewModel convertToNotebookViewModel(Notebook entity) {
+		NotebookViewModel viewModel = new NotebookViewModel();
+		viewModel.setId(entity.getId());
+		viewModel.setName(entity.getName());
+		
+
+		return viewModel;
+	}
+
+	public Notebook convertToNotebookEntity(NotebookViewModel viewModel) {
+		Notebook entity = new Notebook(viewModel.getId(), viewModel.getName());
+
+		return entity;
+	}
+	
+	
+	public FeedbackViewModel convertToFeedbackViewModel(Feedback entity) {
+		FeedbackViewModel viewModel = new FeedbackViewModel();
+		viewModel.setId(entity.getId());
+		viewModel.setName(entity.getName());
+		viewModel.setEmail(entity.getEmail());
+		viewModel.setFeedback(entity.getFeedback());
+
+		return viewModel;
+	}
+
+	public Feedback convertToFeedbackEntity(FeedbackViewModel viewModel) {
+		Feedback entity = new Feedback(viewModel.getId(), viewModel.getName(),viewModel.getEmail(),viewModel.getFeedback());
+
+		return entity;
+	}
+
+}
